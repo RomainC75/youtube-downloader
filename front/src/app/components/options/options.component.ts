9d8aed30a319bf6db764fe25c7622e05c2b9a80e
RomainC75 <rom.chenard@gmail.com>
2023-07-10 23:00:20 +0200

    select

--- front/src/app/components/options/options.component.ts
+++ front/src/app/components/options/options.component.ts
@@ -1,16 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
   styleUrls: ['./options.component.scss']
 })
-export class OptionsComponent {
+export class OptionsComponent implements OnInit {
   @Input('title')title = "";
   @Input('url')url = "";
   @Input('thumbnail')thumbnail = "";
+  selection="video";
+  form!:FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+  }
+
+  ngOnInit(){
+    this.form = this.formBuilder.group({
+      format: [null]
+    })
+  }
 
   download(){
     console.log("==> ", this.title, this.url)
+  }
+
+  submit(){
+    
+    console.log("= value : ", this.form)
   }
 }
